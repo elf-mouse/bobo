6796b72f1ea4ff7a7b245761941da6dc8cbad89e
elf-mouse <ifmouz@gmail.com>
2021-03-05 09:52:19 +0800

    chore: add weeks

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
   const birthday = new Date(2021, 1, 22);
   const today = new Date();
 
-  const date =
+  const days =
     Math.floor((today.getTime() - birthday.getTime()) / (1000 * 60 * 60 * 24)) +
     1;
+  const weeks = Math.ceil(days / 7);
 
-  document.getElementById("bobo").innerHTML = date;
+  document.getElementById("bobo").innerHTML = `${days} days (${weeks} weeks)`;
 })();
