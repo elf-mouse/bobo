8e7b39b5714508f3db12c2e56379e0a2b9a35178
Elf-mousE <ifmouz@gmail.com>
2021-05-05 11:52:48 +0800

    update

--- main.js
+++ main.js
@@ -2,12 +2,17 @@
   const birthday = new Date(2021, 1, 22);
   const today = new Date();
 
-  const days =
-    Math.floor((today.getTime() - birthday.getTime()) / (1000 * 60 * 60 * 24)) +
-    1;
-  const weeks = Math.ceil(days / 7);
+  const days = Math.floor(
+    (today.getTime() - birthday.getTime()) / (1000 * 60 * 60 * 24)
+  );
+  const weeks = Math.floor(days / 7);
+  const weekDays = today.getDay() === 0 ? 6 : today.getDay() - 1;
+  const months = Math.floor(days / 3) / 10;
+
+  const theDays = days + 1;
+  const theWeeks = weekDays ? `${weeks}+${weekDays}` : weeks;
 
   document.getElementById(
     "timeline"
-  ).innerHTML = `${days} days (${weeks} weeks)`;
+  ).innerHTML = `THE ${theDays} DAYS (${days} days | ${theWeeks} weeks | ${months} months)`;
 })();
